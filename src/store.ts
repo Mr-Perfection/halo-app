c4e9c21ad68e64051801d9d5e55ff011d7dc6b26
Stephen Lee <sungsoolee0127@gmail.com>
2022-08-27 14:08:49 -0700

    refactor the auth redux

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { authReducer } from 'components/pages/Auth/authSlice';
 
-export const store = configureStore({ reducer: {} });
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
