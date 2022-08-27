f04575d384c6f769dd558048c54ec7b6f450b479
Stephen Lee <sungsoolee0127@gmail.com>
2022-08-27 14:41:49 -0700

    update store to enable devtools and other setups for redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { authReducer } from 'components/features/Auth/authSlice';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+import { userReducer } from 'components/features/Auth/userSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: userReducer,
   },
+  devTools: process.env.NODE_ENV === 'development',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({}).concat([]),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
