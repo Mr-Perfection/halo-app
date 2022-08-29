fd883360cba53df7b95abf23dc91adbf82b05c88
Stephen Lee <sungsoolee0127@gmail.com>
2022-08-29 10:00:08 -0700

    add customer slug into auth payload; clean up the routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,9 @@ import {
   Routes, Route,
 } from 'react-router-dom';
 
-import { Signup } from 'components/features/Auth';
+import { SignupPage, LoginPage } from 'components/features/Auth';
 import paths from 'constants/nav';
 import { User } from 'generated/graphql';
-import LoginPage from 'components/features/Auth/Login';
 import PrivateRoute from 'components/atoms/PrivateRoute';
 import AdminPage from 'components/features/Admin';
 import AppTemplate from 'components/template/AppTemplate';
@@ -22,15 +21,15 @@ function App() {
       <Routes>
         {/* TODO: Based on permissions, render root page to operator or dashboard. */}
         <Route path={paths.ROOT} element={<PrivateRoute element={<Redirect />} />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route path={paths.ADMIN} element={<PrivateRoute element={<AdminPage />} />} />
+        <Route path={paths.OPERATOR} element={<PrivateRoute element={<OperatorPage />} />} />
         <Route
           path="*"
           element={<NotFound />}
         />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-        <Route path={paths.ADMIN} element={<PrivateRoute element={<AdminPage />} />} />
-        <Route path={paths.OPERATOR} element={<PrivateRoute element={<OperatorPage />} />} />
         {/* <Route path="/admin" element={<AdminPage />} /> */}
       </Routes>
     </AppTemplate>
