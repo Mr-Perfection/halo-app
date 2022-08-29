55fd80c811de8763c7eac8635a79ca4bca8b61bb
Stephen Lee <sungsoolee0127@gmail.com>
2022-08-29 16:46:12 -0700

    remove unused import

--- src/components/molecules/NavItemList.tsx
+++ src/components/molecules/NavItemList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // src
 import ListItemButton from '@mui/material/ListItemButton';
