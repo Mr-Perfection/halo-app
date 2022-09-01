9af905bf6da15675474959a1bcf401a4c2811f97
Stephen Lee <sungsoolee0127@gmail.com>
2022-09-01 00:20:19 -0700

    create user list component (wip) add admin.graphql to UserList

--- src/components/features/UserList/index.tsx
+++ src/components/features/UserList/index.tsx
@@ -1,57 +1,99 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import RefreshIcon from '@mui/icons-material/Refresh';
+import { DataGrid, GridToolbar, GridColDef } from '@mui/x-data-grid';
+import { Paper } from '@mui/material';
+// import { useQuery } from 'urql';
+// import { AdminGetUsersDocument } from 'generated/graphql';
+
+const columns: GridColDef[] = [
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+const rows = [
+  {
+    id: 1, lastName: 'Snow', firstName: 'Jon', age: 35,
+  },
+  {
+    id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42,
+  },
+  {
+    id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45,
+  },
+  {
+    id: 4, lastName: 'Stark', firstName: 'Arya', age: 16,
+  },
+  {
+    id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null,
+  },
+  {
+    id: 6, lastName: 'Melisandre', firstName: null, age: 150,
+  },
+  {
+    id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44,
+  },
+  {
+    id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36,
+  },
+  {
+    id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65,
+  },
+];
 
 export default function UserList() {
+  // const [{fetching, data, error}] = useQuery({
+  //   query: AdminGetUsersDocument,
+  //   variables: {},
+  // });
+  // // TODO: this can be abstracted as some kind for permission layer component.
+  // // const role = currentUser?.role;
+  // // if (role === undefined || role !== UserRole.Admin) {
+  // //   return (<Navigate to={paths.NOT_FOUND} />);
+  // // }
+
+  // const rows =
   return (
-    <Paper sx={{ maxWidth: 936, margin: 'auto', overflow: 'hidden' }}>
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
-      >
-        <Toolbar>
-          <Grid container spacing={2} alignItems="center">
-            <Grid item>
-              <SearchIcon color="inherit" sx={{ display: 'block' }} />
-            </Grid>
-            <Grid item xs>
-              <TextField
-                fullWidth
-                placeholder="Search by email address, phone number, or user UID"
-                InputProps={{
-                  disableUnderline: true,
-                  sx: { fontSize: 'default' },
-                }}
-                variant="standard"
-              />
-            </Grid>
-            <Grid item>
-              <Button variant="contained" sx={{ mr: 1 }}>
-                Add user
-              </Button>
-              <Tooltip title="Reload">
-                <IconButton>
-                  <RefreshIcon color="inherit" sx={{ display: 'block' }} />
-                </IconButton>
-              </Tooltip>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-      <Typography sx={{ my: 5, mx: 2 }} color="text.secondary" align="center">
-        No users for this project yet
-      </Typography>
+    <Paper sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+        components={{ Toolbar: GridToolbar }}
+        componentsProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+      />
     </Paper>
   );
 }
