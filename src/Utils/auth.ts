0dd5df8725df48b84e0fd16c59287db1e2b8be5d
Stephen Lee <sungsoolee0127@gmail.com>
2022-09-05 18:21:51 -0700

    remove comment

--- src/Utils/auth.ts
+++ src/Utils/auth.ts
@@ -4,7 +4,6 @@ import paths from 'constants/nav';
 import { UserRole } from 'generated/graphql';
 import { includes } from 'lodash';
 
-// eslint-disable-next-line import/prefer-default-export
 export const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
 // Only use this if you need to reload the page asfter navigation to login page.
