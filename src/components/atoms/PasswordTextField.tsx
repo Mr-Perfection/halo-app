8e7863360a4219daab1ec80e7420819e30a5aef3
Stephen Lee <sungsoolee0127@gmail.com>
2022-09-06 19:39:18 -0700

    create password text field component

---
+++ src/components/atoms/PasswordTextField.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { TextField, InputAdornment, IconButton } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+export default function PaswordTextField({ label, defaultValue, readOnly }: {
+  label: string, defaultValue: string, readOnly: boolean }) {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = () => setShowPassword(!showPassword);
+
+  return (
+    <TextField
+      label={label}
+      defaultValue={defaultValue}
+      margin="normal"
+      type={showPassword ? 'text' : 'password'}
+      InputProps={{
+        readOnly,
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+            >
+              {showPassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+}
